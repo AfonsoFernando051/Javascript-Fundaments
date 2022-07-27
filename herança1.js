f3f95982269b80014d9d0d0d515e11019f6f2174
AfonsoFernando051 <afonsofernando051@gmail.com>
2022-07-27 14:08:03 -0300

    first commit

---
+++ herança1.js
@@ -0,0 +1,11 @@
+//herança é princípio da OO que permite que você possa reutilizar um código.
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
